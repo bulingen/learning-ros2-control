afdb31b640d7e0064d7b1aee4171ca8056132d85
bulingen <adamlecorney@gmail.com>
2025-07-16 23:35:33 +0200

    feat: executable that runs motor for some seconds

--- brushless_test/src/test_motors.cpp
+++ brushless_test/src/test_motors.cpp
@@ -1,6 +1,25 @@
-#include "rclcpp/rclcpp.hpp"
+// #include "rclcpp/rclcpp.hpp"
+#include "brushless_test/brushless_driver.hpp"
+#include <thread>
+
+using namespace std::chrono_literals;
 
 int main()
 {
+
+    auto driver = BrushlessDriver();
+
+    driver.connect();
+    std::this_thread::sleep_for(1s);
+
+    driver.forward(0.5);
+    std::this_thread::sleep_for(3s);
+    driver.stop();
+    std::this_thread::sleep_for(1s);
+    driver.backward(0.5);
+    std::this_thread::sleep_for(3s);
+
+    driver.disconnect();
+
     return 0;
 }
