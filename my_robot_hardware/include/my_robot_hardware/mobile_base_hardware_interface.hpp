3247df1b4962a442269e05451aca9366f99400a4
bulingen <adamlecorney@gmail.com>
2025-07-19 16:57:28 +0200

    wip: trying to make it work on humble

--- my_robot_hardware/include/my_robot_hardware/mobile_base_hardware_interface.hpp
+++ my_robot_hardware/include/my_robot_hardware/mobile_base_hardware_interface.hpp
@@ -1,6 +1,10 @@
 #ifndef MOBILE_BASE_HARDWARE_INTERFACE_HPP
 #define MOBILE_BASE_HARDWARE_INTERFACE_HPP
 
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/logger.hpp"
+
 #include "hardware_interface/system_interface.hpp"
 #include "my_robot_hardware/PigpioDCMotorDriver.hpp"
 
@@ -30,8 +34,44 @@ namespace mobile_base_hardware
         hardware_interface::return_type
         write(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
+        // Export interfaces
+        std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+        std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
     private:
         std::shared_ptr<PigpioDCMotorDriver> driver_;
+
+        // // Parameters for the DiffBot simulation
+        // double hw_start_sec_;
+        // double hw_stop_sec_;
+
+        // // Objects for logging
+        // std::shared_ptr<rclcpp::Logger> logger_;
+        // rclcpp::Clock::SharedPtr clock_;
+
+        // // Store the command for the simulated robot
+        // std::vector<double> hw_commands_;
+        // std::vector<double> hw_positions_;
+        // std::vector<double> hw_velocities_;
+
+        // States
+        double left_vel_;
+        double right_vel_;
+
+        double left_pos_;
+        double right_pos_;
+
+        std::shared_ptr<rclcpp::Logger> logger_;
+        rclcpp::Clock::SharedPtr clock_;
+
+        // Commands
+        double left_vel_cmd_;
+        double right_vel_cmd_;
+
+        double left_pos_cmd_;
+        double right_pos_cmd_;
+
     }; // class MobileBaseHardwareInterface
 
 } // namespace mobile_base_hardware
--- my_robot_hardware/src/mobile_base_hardware_interface.cpp
+++ my_robot_hardware/src/mobile_base_hardware_interface.cpp
@@ -50,16 +50,44 @@ namespace mobile_base_hardware
         return hardware_interface::CallbackReturn::SUCCESS;
     }
 
+    //     hardware_interface::return_type DiffBotSystemHardware::read(
+    //   const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+    // {
+    //   // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
+    //   std::stringstream ss;
+    //   ss << "Reading states:";
+    //   for (std::size_t i = 0; i < hw_velocities_.size(); i++)
+    //   {
+    //     // Simulate DiffBot wheels's movement as a first-order system
+    //     // Update the joint status: this is a revolute joint without any limit.
+    //     // Simply integrates
+    //     hw_positions_[i] = hw_positions_[i] + period.seconds() * hw_velocities_[i];
+
+    //     ss << std::fixed << std::setprecision(2) << std::endl
+    //        << "\t"
+    //           "position "
+    //        << hw_positions_[i] << " and velocity " << hw_velocities_[i] << " for '"
+    //        << info_.joints[i].name.c_str() << "'!";
+    //   }
+    //   RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 500, "%s", ss.str().c_str());
+    //   // END: This part here is for exemplary purposes - Please do not copy to your production code
+
+    //   return hardware_interface::return_type::OK;
+    // }
+
     hardware_interface::return_type
     MobileBaseHardwareInterface::read(const rclcpp::Time &time, const rclcpp::Duration &period)
     {
         (void)time;
 
         // TODO: need to store this in driver
-        double left_vel = 0.0;
-        double right_vel = 0.0;
-        double left_pos = 0.0;
-        double right_pos = 0.0;
+        // double left_vel = 0.0;
+        // double right_vel = 0.0;
+        // double left_pos = 0.0;
+        // double right_pos = 0.0;
+
+        // hw_positions_[i] = hw_positions_[i] + period.seconds() * hw_velocities_[i];
+        // left_pos_ =
 
         // base_right_wheel_joint
         // base_left_wheel_joint
