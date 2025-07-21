bc1b4d8073d8655fb85aa1689291a8dfc9a07d9e
Adam LeCorney <adamlecorney@gmail.com>
2025-07-21 17:08:00 +0200

    style: formatting

--- custom_robot_teleop/setup.py
+++ custom_robot_teleop/setup.py
@@ -1,23 +1,22 @@
 from setuptools import find_packages, setup
 
-package_name = 'custom_robot_teleop'
+package_name = "custom_robot_teleop"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='bulingen',
-    maintainer_email='adamlecorney@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="bulingen",
+    maintainer_email="adamlecorney@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
         "console_scripts": [
             "cmd_vel_to_single_array = custom_robot_teleop.cmd_vel_to_single_array:main",
